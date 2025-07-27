931d5bc7628d7f85235dff2813c6c3f3954ed0df
pratiksingh001 <singhpratik001@gmail.com>
2025-07-27 17:44:17 +0530

    DashboardNavbar Created

--- src/components/generated-avatar.tsx
+++ src/components/generated-avatar.tsx
@@ -15,24 +15,24 @@ export const GeneratedAvatar = ({
    className,
    variant,
 }: GeneratedAvatarProps) => {
-    let avatar;
+   let avatar;
 
-    if(variant === "botttsNeutral"){
-        avatar = createAvatar(botttsNeutral, {
-            seed
-        })
-    }else{
-        avatar = createAvatar(initials, {
-            seed,
-            fontWeight: 500,
-            fontSize: 42
-        })
-    }
+   if (variant === "botttsNeutral") {
+      avatar = createAvatar(botttsNeutral, {
+         seed,
+      });
+   } else {
+      avatar = createAvatar(initials, {
+         seed,
+         fontWeight: 500,
+         fontSize: 42,
+      });
+   }
 
-    return(
-        <Avatar className={cn(className)}>
-            <AvatarImage src={avatar.toDataUri()} alt = "Avatar"/>
-            <AvatarFallback>{seed.charAt(0).toUpperCase()}</AvatarFallback>
-        </Avatar>
-    )
+   return (
+      <Avatar className={cn(className)}>
+         <AvatarImage src={avatar.toDataUri()} alt="Avatar" />
+         <AvatarFallback>{seed.charAt(0).toUpperCase()}</AvatarFallback>
+      </Avatar>
+   );
 };
