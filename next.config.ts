b657e46223b3838ce18c9b07bce72048d6e29257
pratiksingh001 <singhpratik001@gmail.com>
2025-08-24 14:46:01 +0530

    build issues resolved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   /* config options here */
+  outputFileTracingIncludes: {
+    "/": ["./public/**/*"],
+  },
 };
 
 export default nextConfig;
